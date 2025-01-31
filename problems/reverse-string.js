823ba7e9013f433535bda8426440c647040ae6be
Jeffrey Manning <jeffmgpfit@yahoo.com>
2025-01-31 11:45:14 -0500

    Finishing Phase one

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,11 @@
 module.exports = function reverseString(string) {
-  // Your code here
+  let reversed = ""
+
+  for (i = string.length -1; i >= 0; i--) {
+    let char = string[i];
+
+    reversed += char
+  }
+
+  return reversed
 };
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,44 @@
-// Your code here
+const chai = require("chai");
+const expect = chai.expect;
+
+const reverseString = require("../problems/reverse-string")
+
+describe("Word", function () {
+    let word;
+    
+    beforeEach(() => {
+        word = "hadoken";
+    })
+    
+    describe("reverseString", function () {
+        it("should return reverse the string", function () {
+            const test1 = reverseString(word)
+    
+            expect(test1).to.equal("nekodah")
+        })
+    })
+
+    describe("Throw TypeError", function () {
+        it("should throw a typeError if not a string", function() {
+            let arr = [];
+
+            let num = 4;
+
+            let obj = {};
+
+            const test1 = reverseString(arr);
+
+            const test2 = reverseString(num);
+
+            const test3 = reverseString(obj);
+
+            expect(test1).to.throw(TypeError);
+            expect(test2).to.throw(TypeError);
+            expect(test3).to.throw(TypeError);
+        })
+    })
+  });
+
+
+
+
