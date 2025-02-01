e29af9f3a053f43f6a74eac351acbf020dd11c21
Jeffrey Manning <jeffmgpfit@yahoo.com>
2025-02-01 03:26:22 -0500

    Phase Two Complete

--- problems/number-fun.js
+++ problems/number-fun.js
@@ -1,9 +1,13 @@
 function returnsThree() {
-  // Your code here
+  return 3;
 }
 
 function reciprocal(n) {
-  // Your code here
+  if (n <= 0 || n > 1000000) {
+    throw RangeError("Value is out of range!")
+  }
+
+  return 1/n
 }
 
 module.exports = {
--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -1,1 +1,33 @@
-// Your code here
+const chai = require("chai");
+const expect = chai.expect;
+
+const { returnsThree, reciprocal } = require("../problems/number-fun");
+
+
+describe("returnsThree function", () => {
+    it("should return 3", () => {
+      expect(returnsThree()).to.equal(3);
+    });
+  });
+
+  describe("reciprocal function", () => {
+    context("if arg is within valid range", () => {
+        it("should return the reciprocal number", () => {
+            let test1 = reciprocal(4);
+            let test2 = reciprocal(1/3);
+            let test3 = reciprocal(1.38);
+    
+            expect(test1).to.equal(.25);
+            expect(test2).to.equal(3);
+            expect(test3).to.equal(.7246376811594204);
+        })
+    })
+
+    context("if arg isn't within range", () => {
+        it("should throw a RangeError", () => {
+            expect(() => reciprocal(4000000).to.throw(RangeError));
+            expect(() => reciprocal(-4).to.throw(RangeError));
+        })
+    })
+    
+  })
