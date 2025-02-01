2c0d1ce79ce560d2381a06275b7ea5d0fb7e3a68
Jeffrey Manning <jeffmgpfit@yahoo.com>
2025-02-01 12:22:35 -0500

    Almost done with phase three

--- problems/person.js
+++ problems/person.js
@@ -3,6 +3,18 @@ class Person {
     this.name = name;
     this.age = age;
   }
+
+  sayHello() {
+    return `${this.name} says hello!`
+  }
+
+  visit(person2) {
+    return `${this.name} visits ${person2.name}.`
+  }
+
+  switchVisit(person2) {
+    return person2.visit(this)
+  }
 }
 
 module.exports = Person;
--- test/person-spec.js
+++ test/person-spec.js
@@ -8,17 +8,35 @@ describe("Person", () => {
 
     beforeEach (() => {
         abby = new Person("Abby", 26);
-    })
+        gail = new Person("Gail", 29);
+    });
 
     describe("properties", () => {
         it("should have to 'name' and 'age properties" , () => {
             expect(abby).to.have.all.keys("name", "age");
-        })
+        });
 
         it("should set the 'name' and 'age' properties when a new word is created", () => {
             expect(abby).to.have.property("name");
             expect(abby).to.have.property("age");
+        });
+    });
+
+    describe("sayHello function", () => {
+        it("should return a string with 'name' and greeting message", () => {
+            expect(abby.sayHello()).to.equal("Abby says hello!");
+        });
+    });
+
+    describe("visit function", () => {
+        it("should return string stating this person visited the passed in person", () => {
+            expect(abby.visit(gail)).to.equal("Abby visits Gail.");
+        });
+    });
+
+    describe("switchVisit function", () => {
+        it("should invoke visit() func from person2, use this person as arg", () => {
+            expect(abby.switchVisit(gail)).to.equal("Gail visits Abby.");
         })
     })
-
 })
