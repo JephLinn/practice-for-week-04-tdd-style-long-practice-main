7cc609364c811d80c3427d38dbb0a4be7d60d48a
Jeffrey Manning <jeffmgpfit@yahoo.com>
2025-02-01 04:12:28 -0500

    Phase Three part one complete

--- test/person-spec.js
+++ test/person-spec.js
@@ -1,1 +1,24 @@
-// Your code here
+const chai = require("chai")
+const expect = chai.expect;
+
+const Person = require("../problems/person");
+
+describe("Person", () => {
+    let abby;
+
+    beforeEach (() => {
+        abby = new Person("Abby", 26);
+    })
+
+    describe("properties", () => {
+        it("should have to 'name' and 'age properties" , () => {
+            expect(abby).to.have.all.keys("name", "age");
+        })
+
+        it("should set the 'name' and 'age' properties when a new word is created", () => {
+            expect(abby).to.have.property("name");
+            expect(abby).to.have.property("age");
+        })
+    })
+
+})
