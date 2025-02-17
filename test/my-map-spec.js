60e16688a20fe55eb7d601ea3f038aa8af382fb6
Jeffrey Manning <jeffmgpfit@yahoo.com>
2025-02-17 12:44:42 -0500

    Starting Bonus

--- test/my-map-spec.js
+++ test/my-map-spec.js
@@ -1,1 +1,13 @@
-// Your code here
+const chai = require("chai");
+const { beforeEach } = require("mocha");
+const expect = chai.expect;
+
+// const { my-map } = require("../problems/my-map");
+
+// describe("my-map", () => {
+//     beforeEach(() => {
+//         const arr = [1, 2, 3];
+
+//         const callback = (el) => el * 2;
+//     });
+// })
